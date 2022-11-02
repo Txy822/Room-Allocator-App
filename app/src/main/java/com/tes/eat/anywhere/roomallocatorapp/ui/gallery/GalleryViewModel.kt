1bc5cd02fb025378dd9ffe98ddd0a961e1f39a6f
tesfahun <y.w.tesfahun@gmail.com>
2022-11-02 11:52:13 +0000

    Initial commit

---
+++ app/src/main/java/com/tes/eat/anywhere/roomallocatorapp/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.tes.eat.anywhere.roomallocatorapp.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
